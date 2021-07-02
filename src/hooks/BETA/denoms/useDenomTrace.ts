7086623a76adfb38be9435e188d8ff848180db90
sko-kr <vxxcko@gmail.com>
2021-07-03 03:50:04 +0900

    refactor: denomTrace remote only accepts ibcDenoms

--- src/hooks/BETA/denoms/useDenomTrace.ts
+++ src/hooks/BETA/denoms/useDenomTrace.ts
@@ -2,12 +2,20 @@ import { useQuery } from 'react-query';
 import { getDenomTrace } from '../../../remotes/denoms/getDenomTrace';
 
 interface Params {
-	coinMinimalDenom: string;
+	ibcDenom?: string;
+	/** restDomain is always OSMOSIS_CHAIN_API */
 	restDomain?: string;
 }
 
-export function useDenomTrace({ coinMinimalDenom, restDomain }: Params) {
-	return useQuery(['denomTrace', restDomain, coinMinimalDenom], () => {
-		return getDenomTrace({ coinMinimalDenom, restDomain });
-	});
+export function useDenomTrace({ ibcDenom, restDomain }: Params) {
+	return useQuery(
+		['denomTrace', restDomain, ibcDenom],
+		() => {
+			if (ibcDenom == null) {
+				return null;
+			}
+			return getDenomTrace({ ibcDenom, restDomain });
+		},
+		{ enabled: ibcDenom != null }
+	);
 }
--- src/remotes/denoms/getDenomTrace.ts
+++ src/remotes/denoms/getDenomTrace.ts
@@ -1,15 +1,26 @@
 import axios from 'axios';
 import { OSMOSIS_CHAIN_API_DOMAIN } from '../../constants/BETA/urls';
 
+interface DenomTraceRes {
+	/** native coin, eg) uatom */
+	base_denom: string;
+	/** contractAddress/osmosisChannelId eg) transfer/channel-0 */
+	path: string;
+}
+
 interface Params {
-	/** can be ibcToken eg) ibc/XXX, or nativeToken eg) uatom */
-	coinMinimalDenom: string;
+	/** can be ibcToken eg) ibc/XXX, or  eg) uatom */
+	ibcDenom: string;
+	/** TODO:Q it seems restDomain is always OSMOSIS_CHAIN_API_DOMAIN?*/
 	restDomain?: string;
 }
 
-export async function getDenomTrace({ coinMinimalDenom, restDomain = OSMOSIS_CHAIN_API_DOMAIN }: Params) {
-	const coinMinimalDenomWithoutPrefix = coinMinimalDenom.replace('ibc/', '');
-	const res = await axios.get(
+export async function getDenomTrace({ ibcDenom, restDomain = OSMOSIS_CHAIN_API_DOMAIN }: Params) {
+	if (!ibcDenom.startsWith('ibc/')) {
+		return null;
+	}
+	const coinMinimalDenomWithoutPrefix = ibcDenom.replace('ibc/', '');
+	const res = await axios.get<{ denom_trace: DenomTraceRes }>(
 		`${restDomain}/ibc/applications/transfer/v1beta1/denom_traces/${coinMinimalDenomWithoutPrefix}`
 	);
 	return res.data;
