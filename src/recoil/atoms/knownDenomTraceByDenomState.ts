6f232d8be3add6caf433360f90a4572d91ac6695
sko-kr <vxxcko@gmail.com>
2021-07-07 13:30:49 +0900

    refactor: change known denom trace.

---
+++ src/recoil/atoms/knownDenomTraceByDenomState.ts
@@ -0,0 +1,10 @@
+import { atom } from 'recoil';
+import { DenomTraceRes } from '../../models/BETA/denom';
+
+/** save knownDenomTrace as new pools are queried.
+ * If unknown denom trace exists, such denom should not be shown in frontend app.
+ * */
+export const knownDenomTraceByDenomState = atom({
+	key: 'knownDenomTraceByDenomState',
+	default: {} as { [ibcDenom: string]: DenomTraceRes | null | undefined },
+});
